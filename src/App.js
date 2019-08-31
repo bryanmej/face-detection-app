099203f8f48cc3033e3b24f5b50cd90e44cfdd46
bryanmej <bryanmej@gmail.com>
2019-08-31 03:12:02 -0500

    face-recognition component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Navigation from "./components/Navigation";
 import "./App.css";
 import Logo from "./components/Logo";
 import ImageLinkForm from "./components/imageLinkForm";
+import FaceRecognition from "./components/FaceRecognition";
 import Rank from "./components/Rank";
 import Particles from "react-particles-js";
+// import Clarifai from "clarifai";
+
+// const app = new Clarifai.App({
+//   apiKey: "7e9f9f99be7a46aa87ac637652fe7fe0"
+// });
 
 const particleOptions = {
   particles: {
@@ -19,13 +25,47 @@ const particleOptions = {
 };
 
 function App() {
+  const [state, setInput] = useState({});
+
+  // const handleInput = e => {
+  //   setInput({
+  //     input: e.target.value
+  //   });
+  //   console.log(e.target.value);
+  // };
+
+  const handleInput = e => {
+    e.persist();
+    setInput(prevState => ({
+      ...prevState,
+      [e.target.value]: e.target.value
+    }));
+  };
+
+  const onSubmit = e => {
+    console.log(state);
+    //   app.models
+    //     .predict(
+    //       Clarifai.COLOR_MODEL,
+    //       // URL
+    //       "https://samples.clarifai.com/metro-north.jpg"
+    //     )
+    //     .then(
+    //       function(response) {
+    //         console.log(response);
+    //       },
+    //       function(err) {}
+    //     );
+  };
+
   return (
     <div className="App">
       <Particles className="particles" params={particleOptions} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm handleInput={handleInput} onSubmit={onSubmit} />
+      <FaceRecognition />
     </div>
   );
 }
---
+++ src/components/FaceRecognition.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const FaceRecognition = () => {
+  return (
+    <div className="center">
+      <img
+        src="https://samples.clarifai.com/metro-north.jpg"
+        alt="recognition"
+      />
+    </div>
+  );
+};
+
+export default FaceRecognition;
